1a36f54718efcb663156951acffbdc2888f8a44a
Andrew236 <deckerstrom@ymail.com>
2020-08-04 21:07:59 -0700

    added build

---
+++ client/build/precache-manifest.87ba807ec1c88705c83cf2a5f1883e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008a34732367b03f09050761a215aea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92ce5a69b26483d69c27",
+    "url": "/static/css/main.d1c1beb3.chunk.css"
+  },
+  {
+    "revision": "c045f1f4c7a29ee6dca2",
+    "url": "/static/js/2.c16adf07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c16adf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ce5a69b26483d69c27",
+    "url": "/static/js/main.3bdf78bf.chunk.js"
+  },
+  {
+    "revision": "e9f049c8caaf64569c32",
+    "url": "/static/js/runtime-main.0007566e.js"
+  }
+]);
